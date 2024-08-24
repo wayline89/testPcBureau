981055206de6ee843ee9a4290f92323a6815330e
wayline89 <laurentdugardin@gmail.com>
2024-08-25 00:58:00 +0200

    debut des test

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+<h1 className='bg-red-400 text-yellow-300'>dsfhfhfdsqhsdfhdfhsdh</h1>
+    </div>
+  )
+}
+
+export default App
